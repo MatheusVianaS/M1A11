5cd18b9dfaf814f20dd8bdbb29057fb1cc458cc6
MatheusVianaS <49397317+MatheusVianaS@users.noreply.github.com>
2019-04-21 18:35:02 -0300

    Add files via upload

---
+++ react-webpack2/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div className='container'>
+        <Title name='Matheus' lastname='Viana' />
+        </div>
+    )
+  }
+})
+  
+export default App
